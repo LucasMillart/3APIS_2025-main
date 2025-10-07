233da8e55d87a67991c0181c5b61daeef30f96d4
LucasMillart <lucas.millart@gmail.com>
2025-10-07 14:23:31 +0200

    initialisation TP3

---
+++ Day 4/src/routers/users-router.js
@@ -0,0 +1,35 @@
+import express from "express";
+
+import { User } from "../mongo.js";
+
+const router = express.Router();
+
+router.get("/", async (request, response) =>
+{
+  const users = await User.find();
+  response.status(200).json(users);
+});
+
+router.get("/:id", async (request, response) =>
+{
+  const user = await User.findById(request.params.id);
+  response.status(200).json(user);
+});
+
+router.post('/', (request, response) =>
+{
+  const newUser = User({ ...request.body });
+  newUser.save()
+    .then(
+      user =>
+      {
+        response.status(200).json({ message: `Bienvenue ${user.username}, ton compte a été créé avec succès. Tu peux te connecter !`, id: newUser._id })
+      }
+    ).catch(error =>
+    {
+      console.log("Erreur lors de la création de compte");
+      response.status(500).message({ message: "Erreur lors de la création de compte" });
+    });
+});
+
+export default router;
